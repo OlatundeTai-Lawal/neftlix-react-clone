df774f1634d35d65b9c480ca9f47fda8c81a7c79
Olatunde <tundeficky@gmail.com>
2023-08-25 18:38:24 -0700

    ecs file updated

--- ecs_main.tf
+++ ecs_main.tf
@@ -1,26 +1,26 @@
 # Define the provider for AWS
 provider "aws" {
-  region = "eu-west-2"  
+  region = "us-east-1"
 }
 
 # Create an ECS cluster
 resource "aws_ecs_cluster" "my_cluster" {
-  name = "karo-ecs-cluster"  
+  name = "ficky-cluster"
 }
 
 # Create a task definition
 resource "aws_ecs_task_definition" "my_task_definition" {
-  family                = "my-task-family-test"
+  family                   = "my-task-family-test"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
   memory                   = 2048
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions = <<EOF
+  container_definitions    = <<EOF
 [
   {
     "name": "my-container",
-    "image": "335871625378.dkr.ecr.eu-west-2.amazonaws.com/netflix-app:latest",  
+    "image": "544795736365.dkr.ecr.us-east-1.amazonaws.com/netflix-clone-app",  
     "portMappings": [
       {
         "containerPort": 80,
@@ -40,14 +40,14 @@ resource "aws_ecs_service" "my_service" {
   desired_count   = 1
   launch_type     = "FARGATE"
   network_configuration {
-    subnets          = ["subnet-0e8b3d7199bf251a4"]  
-    security_groups  = ["sg-0dab44e5556cb4879"]      
+    subnets          = ["subnet-0fcd5d1b7058a851f"]
+    security_groups  = ["sg-0cb468e12be682b15"]
     assign_public_ip = true
   }
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
+  name               = "ecs_task_execution_role"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -68,3 +68,4 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
   role       = aws_iam_role.ecs_task_execution_role.name
 }
+
